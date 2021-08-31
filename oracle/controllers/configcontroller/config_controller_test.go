35516ef66a178efba2ae487550c6a2811962675e
Ash Gbadamassi <gbadamassi@google.com>
2021-08-31 10:20:17 -0400

    Modify the Config controller to support multiple instances
    Tested the changes in a live cluster and found them to be working. Relevant integration tests will be added in a separate CL.
    
    bug: 194809075
    Change-Id: I352ffa07a8a24eb5838479d118842121e9de3668

--- oracle/controllers/configcontroller/config_controller_test.go
+++ oracle/controllers/configcontroller/config_controller_test.go
@@ -16,6 +16,8 @@ package configcontroller
 
 import (
 	"context"
+	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -25,12 +27,23 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	v1alpha1 "github.com/GoogleCloudPlatform/elcarro-oracle-operator/oracle/api/v1alpha1"
+	"github.com/GoogleCloudPlatform/elcarro-oracle-operator/oracle/api/v1alpha1"
 	"github.com/GoogleCloudPlatform/elcarro-oracle-operator/oracle/controllers/testhelpers"
 )
 
 var k8sClient client.Client
 var k8sManager ctrl.Manager
+
+// Define utility constants for object names and testing timeouts and intervals.
+const (
+	Namespace  = "default"
+	ConfigName = "test-config"
+
+	timeout  = time.Second * 15
+	interval = time.Millisecond * 250
+
+	instanceCount = 3
+)
 
 func TestConfigController(t *testing.T) {
 	testhelpers.RunReconcilerTestSuite(t, &k8sClient, &k8sManager, "Config controller", func() []testhelpers.Reconciler {
@@ -46,50 +59,64 @@ func TestConfigController(t *testing.T) {
 }
 
 var _ = Describe("Config controller", func() {
-	// Define utility constants for object names and testing timeouts and intervals.
-	const (
-		Namespace  = "default"
-		ConfigName = "test-config"
-
-		timeout  = time.Second * 15
-		interval = time.Millisecond * 250
-	)
-
+	var patchedDeploymentCount uint32
 	config := &v1alpha1.Config{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: Namespace,
 			Name:      ConfigName,
 		},
 	}
+	configObjectKey := client.ObjectKey{Namespace: Namespace, Name: ConfigName}
 
-	var reconciler ConfigReconciler
 	BeforeEach(func() {
-		reconciler = ConfigReconciler{
+		Patch = func(reconciler *ConfigReconciler, ctx context.Context, object client.Object, patch client.Patch, option ...client.PatchOption) error {
+			atomic.AddUint32(&patchedDeploymentCount, 1)
+			return nil
+		}
+	})
+
+	It("Should succeed when config exists", func() {
+		createInstances()
+		Expect(k8sClient.Create(context.Background(), config)).Should(Succeed())
+
+		createdConfig := &v1alpha1.Config{}
+		Eventually(func() bool {
+			err := k8sClient.Get(context.Background(), configObjectKey, createdConfig)
+			return err == nil
+		}, timeout, interval).Should(BeTrue())
+
+		Eventually(func() bool {
+			// The Patch function is called once for each deployment that should be patched.
+			return atomic.LoadUint32(&patchedDeploymentCount) == instanceCount
+		}, timeout, interval).Should(BeTrue())
+
+		Expect(k8sClient.Delete(context.Background(), config)).Should(Succeed())
+	})
+
+	It("Should succeed when config doesn't exist", func() {
+		reconciler := ConfigReconciler{
 			Client: k8sClient,
 			Log:    ctrl.Log,
 			Scheme: k8sManager.GetScheme(),
 			Images: map[string]string{"config": "config_image"},
 		}
-	})
-
-	objKey := client.ObjectKey{Namespace: Namespace, Name: ConfigName}
-	It("Should success when config exists", func() {
-		Expect(k8sClient.Create(context.Background(), config)).Should(Succeed())
-
-		createdConfig := &v1alpha1.Config{}
-		Eventually(func() bool {
-			err := k8sClient.Get(context.Background(), client.ObjectKey{Namespace: Namespace, Name: ConfigName}, createdConfig)
-			return err == nil
-		}, timeout, interval).Should(BeTrue())
-
-		_, err := reconciler.Reconcile(context.Background(), ctrl.Request{NamespacedName: objKey})
-		Expect(err).ToNot(HaveOccurred())
-
-		Expect(k8sClient.Delete(context.Background(), config)).Should(Succeed())
-	})
-
-	It("Should success when config doesn't exist", func() {
-		_, err := reconciler.Reconcile(context.Background(), ctrl.Request{NamespacedName: objKey})
+		// Force the reconciler to run since there's no state change in this test spec that would cause it to run.
+		_, err := reconciler.Reconcile(context.Background(), ctrl.Request{NamespacedName: configObjectKey})
 		Expect(err).ToNot(HaveOccurred())
 	})
 })
+
+func createInstances() {
+	for i := 0; i < instanceCount; i++ {
+		instance := &v1alpha1.Instance{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("myinstance-%d", i),
+				Namespace: Namespace,
+			},
+			Spec: v1alpha1.InstanceSpec{
+				CDBName: fmt.Sprintf("MYDB-%d", i),
+			},
+		}
+		Expect(k8sClient.Create(context.Background(), instance)).Should(Succeed())
+	}
+}
