d9fbeab582cee1375bcab71ae8e502c3792a7273
Kurt Kartaltepe <kkartaltepe@google.com>
2022-10-19 14:40:16 -0700

    Fixup install script to avoid prompts
    We were using apt which may prompt, instead heed the warning and use
    apt-get and pass -y to agree to any potential prompts. Fixes presubmits.
    
    Change-Id: I4c6f7a77db7b70582f239cafe79eb413284a6fd6

--- oracle/scripts/install_prow_deps.sh
+++ oracle/scripts/install_prow_deps.sh
@@ -36,7 +36,7 @@ INSTALL_TMP_DIR=$(mktemp -d)
 cd $INSTALL_TMP_DIR
 
 # upgrade bazel
-apt install apt-transport-https curl gnupg
+apt-get install -y apt-transport-https curl gnupg
 curl -fsSL https://storage.googleapis.com/www.bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
