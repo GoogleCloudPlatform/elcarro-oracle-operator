868e88b9ed29842bf8a1342b2d60ac1eabd562ae
Konstantin Chernyshev <kchernyshev@google.com>
2024-02-07 15:46:52 -0800

    fix: increase timeout for database deletion in instance_test
    Change-Id: I0ee9dd9f8c8545d9016c4ff8c309caa30f83979b

--- oracle/controllers/inttest/instancetest/instance_test.go
+++ oracle/controllers/inttest/instancetest/instance_test.go
@@ -289,7 +289,7 @@ var _ = Describe("Instance and Database provisioning", func() {
 				updatedInstance := &v1alpha1.Instance{}
 				Expect(k8sClient.Get(ctx, client.ObjectKey{Name: firstInstanceName, Namespace: namespace}, updatedInstance)).Should(Succeed())
 				return updatedInstance.Status.DatabaseNames, nil
-			}, 2*time.Minute, 5*time.Second).Should(BeEmpty())
+			}, 10*time.Minute, 5*time.Second).Should(BeEmpty())
 
 			By("Checking that only PVCs for the first instance are retained")
 			deleteInstance(ctx, firstInstanceName, namespace)
